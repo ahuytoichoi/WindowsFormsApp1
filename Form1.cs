d643b6402598e87d03cc6d44d334c910bdd754f9
Administrator <Administrator@17-LAB01>
2025-06-30 09:33:58 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+
+    {
+        string constring = "Data Source=18-LAB01\\MISASME2022;Initial Catalog=QuanLyDiemSV;Integrated Security=True;Connect Timeout=30;Encrypt=False";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(constring))
+                {
+                    conn.Open();
+                    string query = "SELECT TenMH FROM MONHOC";
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi kết nối SQL Server: " + ex.Message);
+            }
+        }
+    }
+}
